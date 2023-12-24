7b512208af2adaa556683c04d9548e2432966170
LetsWorkAround <peene_dev@naver.com>
2023-12-25 02:12:35 +0900

    Revert "Revert "sports selecting is adding""
    This reverts commit ce2b1750107600368f6e14bee63dcb35d65befdd.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import FormNewPost from "@/components/form-new-post";
 import AllBlogs from "./blogs/AllBlogs";
 import MostLikedBlogs from "./blogs/MostLikedBlogs";
+import SportsSection from "@/components/SportsSection";
 
 export default async function Home() {
     return (
         <main className="p-8 space-y-5">
+            <SportsSection />
             <AllBlogs />
             <MostLikedBlogs />
         </main>
