f70162b047ff701f2c07eac905597c4523475295
LetsWorkAround <peene_dev@naver.com>
2024-01-01 01:51:00 +0900

    g auth2

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,14 +9,14 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-            authorization: {
-                params: {
-                    response_type: "code",
-                    redirect_uri:
-                        "http:///www.sportsforlife.co.kr/api/auth/signin",
-                    scope: "https://www.googleapis.com/auth/signin",
-                },
-            },
+            // authorization: {
+            //     params: {
+            //         response_type: "code",
+            //         redirect_uri:
+            //             "http:///www.sportsforlife.co.kr/api/auth/signin",
+            //         scope: "https://www.googleapis.com/auth/signin",
+            //     },
+            // },
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
