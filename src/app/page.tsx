a61f166762e5ed6478ae465d6d30a474e298b899
LetsWorkAround <peene_dev@naver.com>
2024-01-01 20:17:01 +0900

    gtag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import AllBlogs from "../components/Board/AllBlogs";
 import MostLikedBlogs from "../components/Board/MostLikedBlogs";
 import SportsSection from "@/components/SportsSection";
@@ -5,6 +6,16 @@ import SportsSection from "@/components/SportsSection";
 export default async function Home() {
     return (
         <main className="p-8 space-y-5">
+            <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
+            <Script id="google-analytics">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+        
+                gtag('config', 'G-KYBD2P8KTB');
+            `}
+            </Script>
             <SportsSection />
             <AllBlogs
                 params={{
